77980f8260d4f50494b00791e41ca9052c1a5cae
lintianlin <mhjdxlwb@sina.com>
2017-03-09 14:48:18 +0800

    a simple rich editor

---
+++ SimpleText/app/src/main/java/com/jackma/simpletext/MainActivity.java
@@ -0,0 +1,99 @@
+package com.jackma.simpletext;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.jackma.simplericheditor.RichEditor;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private RichEditor mEditor;
+    private TextView mPreview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mEditor = (RichEditor) findViewById(R.id.editor);
+        mEditor.setEditorHeight(200);
+        mEditor.setEditorFontSize(18);
+        mEditor.setEditorFontColor(Color.BLACK);
+        //mEditor.setEditorBackgroundColor(Color.BLUE);
+        //mEditor.setBackgroundColor(Color.BLUE);
+        //mEditor.setBackgroundResource(R.drawable.bg);
+        mEditor.setPadding(10, 10, 10, 10);
+        mEditor.setPlaceholder("Insert text here...");
+
+        mPreview = (TextView) findViewById(R.id.preview);
+        mEditor.setOnTextChangeListener(new RichEditor.OnTextChangeListener() {
+            @Override
+            public void onTextChange(String text) {
+                mPreview.setText(text);
+                Log.e("mPreview", text);
+            }
+        });
+
+        findViewById(R.id.action_undo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEditor.undo();
+            }
+        });
+
+        findViewById(R.id.action_redo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEditor.redo();
+            }
+        });
+
+        findViewById(R.id.action_bold).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEditor.setBold();
+            }
+        });
+
+        findViewById(R.id.action_italic).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEditor.setItalic();
+            }
+        });
+
+        findViewById(R.id.action_underline).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEditor.setUnderline();
+            }
+        });
+
+
+        findViewById(R.id.action_align_left).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEditor.setAlignLeft();
+            }
+        });
+
+        findViewById(R.id.action_align_center).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEditor.setAlignCenter();
+            }
+        });
+
+        findViewById(R.id.action_align_right).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEditor.setAlignRight();
+            }
+        });
+
+    }
+}
---
+++ SimpleText/simplericheditor/src/main/assets/rich_editor.js
@@ -0,0 +1,215 @@
+
+var RE = {};
+
+RE.currentSelection = {
+    "startContainer": 0,
+    "startOffset": 0,
+    "endContainer": 0,
+    "endOffset": 0};
+
+RE.editor = document.getElementById('editor');
+
+document.addEventListener("selectionchange", function() { RE.backuprange(); });
+
+// Initializations
+RE.callback = function() {
+    window.location.href = "re-callback://" + encodeURI(RE.getHtml());
+}
+
+RE.setHtml = function(contents) {
+    RE.editor.innerHTML = decodeURIComponent(contents.replace(/\+/g, '%20'));
+}
+
+RE.getHtml = function() {
+    return RE.editor.innerHTML;
+}
+
+RE.getText = function() {
+    return RE.editor.innerText;
+}
+
+RE.setBaseTextColor = function(color) {
+    RE.editor.style.color  = color;
+}
+
+RE.setBaseFontSize = function(size) {
+    RE.editor.style.fontSize = size;
+}
+
+RE.setPadding = function(left, top, right, bottom) {
+  RE.editor.style.paddingLeft = left;
+  RE.editor.style.paddingTop = top;
+  RE.editor.style.paddingRight = right;
+  RE.editor.style.paddingBottom = bottom;
+}
+
+RE.setBackgroundColor = function(color) {
+    document.body.style.backgroundColor = color;
+}
+
+RE.setBackgroundImage = function(image) {
+    RE.editor.style.backgroundImage = image;
+}
+
+RE.setWidth = function(size) {
+    RE.editor.style.minWidth = size;
+}
+
+RE.setHeight = function(size) {
+    RE.editor.style.height = size;
+}
+
+RE.setTextAlign = function(align) {
+    RE.editor.style.textAlign = align;
+}
+
+RE.setVerticalAlign = function(align) {
+    RE.editor.style.verticalAlign = align;
+}
+
+RE.setPlaceholder = function(placeholder) {
+    RE.editor.setAttribute("placeholder", placeholder);
+}
+
+RE.undo = function() {
+    document.execCommand('undo', false, null);
+}
+
+RE.redo = function() {
+    document.execCommand('redo', false, null);
+}
+
+RE.setBold = function() {
+    document.execCommand('bold', false, null);
+}
+
+RE.setItalic = function() {
+    document.execCommand('italic', false, null);
+}
+
+
+RE.setUnderline = function() {
+    document.execCommand('underline', false, null);
+}
+
+RE.setJustifyLeft = function() {
+    document.execCommand('justifyLeft', false, null);
+}
+
+RE.setJustifyCenter = function() {
+    document.execCommand('justifyCenter', false, null);
+}
+
+RE.setJustifyRight = function() {
+    document.execCommand('justifyRight', false, null);
+}
+
+
+RE.insertHTML = function(html) {
+    RE.restorerange();
+    document.execCommand('insertHTML', false, html);
+}
+
+RE.insertLink = function(url, title) {
+    RE.restorerange();
+    var sel = document.getSelection();
+    if (sel.toString().length == 0) {
+        document.execCommand("insertHTML",false,"<a href='"+url+"'>"+title+"</a>");
+    } else if (sel.rangeCount) {
+       var el = document.createElement("a");
+       el.setAttribute("href", url);
+       el.setAttribute("title", title);
+
+       var range = sel.getRangeAt(0).cloneRange();
+       range.surroundContents(el);
+       sel.removeAllRanges();
+       sel.addRange(range);
+   }
+    RE.callback();
+}
+
+
+
+RE.prepareInsert = function() {
+    RE.backuprange();
+}
+
+RE.backuprange = function(){
+    var selection = window.getSelection();
+    if (selection.rangeCount > 0) {
+      var range = selection.getRangeAt(0);
+      RE.currentSelection = {
+          "startContainer": range.startContainer,
+          "startOffset": range.startOffset,
+          "endContainer": range.endContainer,
+          "endOffset": range.endOffset};
+    }
+}
+
+RE.restorerange = function(){
+    var selection = window.getSelection();
+    selection.removeAllRanges();
+    var range = document.createRange();
+    range.setStart(RE.currentSelection.startContainer, RE.currentSelection.startOffset);
+    range.setEnd(RE.currentSelection.endContainer, RE.currentSelection.endOffset);
+    selection.addRange(range);
+}
+
+RE.enabledEditingItems = function(e) {
+    var items = [];
+    if (document.queryCommandState('bold')) {
+        items.push('bold');
+    }
+    if (document.queryCommandState('italic')) {
+        items.push('italic');
+    }
+    if (document.queryCommandState('underline')) {
+        items.push('underline');
+    }
+    if (document.queryCommandState('justifyCenter')) {
+        items.push('justifyCenter');
+    }
+    if (document.queryCommandState('justifyFull')) {
+        items.push('justifyFull');
+    }
+    if (document.queryCommandState('justifyLeft')) {
+        items.push('justifyLeft');
+    }
+    if (document.queryCommandState('justifyRight')) {
+        items.push('justifyRight');
+    }
+    var formatBlock = document.queryCommandValue('formatBlock');
+    if (formatBlock.length > 0) {
+        items.push(formatBlock);
+    }
+
+    window.location.href = "re-state://" + encodeURI(items.join(','));
+}
+
+RE.focus = function() {
+    var range = document.createRange();
+    range.selectNodeContents(RE.editor);
+    range.collapse(false);
+    var selection = window.getSelection();
+    selection.removeAllRanges();
+    selection.addRange(range);
+    RE.editor.focus();
+}
+
+RE.blurFocus = function() {
+    RE.editor.blur();
+}
+
+RE.removeFormat = function() {
+    execCommand('removeFormat', false, null);
+}
+
+// Event Listeners
+RE.editor.addEventListener("input", RE.callback);
+RE.editor.addEventListener("keyup", function(e) {
+    var KEY_LEFT = 37, KEY_RIGHT = 39;
+    if (e.which == KEY_LEFT || e.which == KEY_RIGHT) {
+        RE.enabledEditingItems(e);
+    }
+});
+RE.editor.addEventListener("click", RE.enabledEditingItems);
